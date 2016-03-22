aa1668a9764da470ff72d8adc791ca5aff2d49a0
Michael Beckwith <tw2113@gmail.com>
2016-03-22 13:40:19 -0500

    check for image existence and if they are gone, fall back to basic link

--- inc/utility.php
+++ inc/utility.php
@@ -263,7 +263,7 @@ function cptui_products_sidebar() {
 	if ( false === ( $ads = get_transient( 'wds_promos' ) ) ) {
 		$ads = wp_remote_get( 'https://webdevstudios.com/assets/wds.json' );
 
-		if ( '200' == wp_remote_retrieve_response_code( $ads ) ) {
+		if ( 200 === wp_remote_retrieve_response_code( $ads ) ) {
 			$ads = json_decode( wp_remote_retrieve_body( $ads ) );
 			set_transient( 'wds_promos', $ads, DAY_IN_SECONDS );
 		}
@@ -272,14 +272,24 @@ function cptui_products_sidebar() {
 	if ( ! empty( $ads ) ) {
 		echo '<div class="wdspromos">';
 		foreach ( $ads as $ad ) {
+			$the_ad = $ad->text;
+			$image = wp_remote_get( $ad->image );
+			if ( 200 === wp_remote_retrieve_response_code( $image ) ) {
+				$the_ad = sprintf(
+					'<img src="%s" alt="%s">',
+					$ad->image,
+					$ad->text
+				);
+			}
+
 			printf(
-				'<a href="%s"><img src="%s" alt="%s"></a>',
+				'<a href="%s">%s</a>',
 				$ad->url,
-				$ad->image,
-				$ad->text
+				$the_ad
 			);
 		}
 		echo '</div>';
+
 	}
 
 }
